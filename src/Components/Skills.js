c43d98befdb8e782dcfdfae0608dacd80067b4b4
AldousMaranga <marangaaldous263@gmail.com>
2024-05-02 19:07:20 +0800

    added skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -2,9 +2,33 @@ import React, { Component } from 'react';
 
 class Skills extends Component {
     render() {
-        return(
-            <div style={{paddingTop: '80px'}} className='container'>
-                Skills
+        return (
+            <div style={{ paddingTop: '80px' }} className='container'>
+                <div className='skills-cont'>
+                    <div className='header'>Skills Learned</div>
+                    <div className='header1'>Some of the projects I've developed through my career.</div>
+
+                    <div className='skill-column'>
+                        <div className='skill-card'>
+                            Hi
+                        </div>
+                        <div className='skill-card'>
+                            Hi
+                        </div>
+                        <div className='skill-card'>
+                            Hi
+                        </div>
+                        <div className='skill-card'>
+                            Hi
+                        </div>
+                        <div className='skill-card'>
+                            Hi
+                        </div>
+                        <div className='skill-card'>
+                            Hi
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
